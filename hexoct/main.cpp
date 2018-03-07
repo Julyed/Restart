991d187797e8327c0d45297810fcb380d3c44ab3
Julyed <274922025@qq.com>
2018-03-07 14:07:20 +0800

    十进制八进制十六进制的表示和输出
    Signed-off-by: Julyed <274922025@qq.com>

---
+++ hexoct/main.cpp
@@ -0,0 +1,23 @@
+// hexoct1.cpp -- shows hex and octal literals
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int chest = 42;
+    int waist = 0x42;
+    int inseam = 042;
+
+    cout << "Monsieur cuts a striking figure!\n";
+    cout << "chest = " << chest << " (42 in decimal)\n";
+    cout << "waist = " << waist << " (0x42 in hex)\n";
+    cout << "inseam = " << inseam << " (042 in octal)\n";
+
+    cout << endl << endl;
+    cout << hex;
+    cout << "waist = " << waist << " (hexadecimal for 42)" << endl;
+    cout << oct;
+    cout << "inseam = " << inseam << " (octal for 42)" << endl;
+    return 0;
+}
