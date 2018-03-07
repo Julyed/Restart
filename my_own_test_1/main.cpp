9e70c45e61c8a623caaeef3af820ea463acd863f
Julyed <274922025@qq.com>
2018-03-07 19:45:06 +0800

    更新：测试wchar_t
    Signed-off-by: Julyed <274922025@qq.com>

--- my_own_test_1/main.cpp
+++ my_own_test_1/main.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 int main()
 {
-    // ’Ò¡Â
-    char alarm = '\a';
-    cout << alarm << "Don't do that again!\a\n";
-    cout << "Ben \"Buggsie\" Hacker\nwas here!\n";
-
-    // ctr + z
-    cout << "hi\x1a there" << endl;
+    wchar_t bob = L'P';     // a wide-character constant
+    wcout << L"tall" << " " << bob << endl;   //outputting a wide-character string
     return 0;
 }
