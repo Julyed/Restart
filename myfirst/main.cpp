1fc89604d2d7740824c461863fdf355a19408818
Julyed <274922025@qq.com>
2018-03-09 16:37:33 +0800

    教材中第一个程序
    Signed-off-by: Julyed <274922025@qq.com>

---
+++ myfirst/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cin.get();
+    cout << "Hello world!";
+    cout << endl;
+    cout << "I won't regret it." << endl;
+    return 0;
+}
