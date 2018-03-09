7a2fb0dad587d653f7e9706ea552999784882ff9
Julyed <274922025@qq.com>
2018-03-09 19:23:41 +0800

    枚举类型的定义与赋值
    Signed-off-by: Julyed <274922025@qq.com>

--- my_own_test_1/main.cpp
+++ my_own_test_1/main.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
 using namespace std;
 
-struct widget
-{
-    char brand[20];
-    int type;
-    union id
-    {
-        long id_num;
-        char id_char[20];
-    }id_val;
-};
 int main()
 {
-    widget prize;
-    cout << "Please enter the brand and the id type:\n";
-    cout << "brand: ";
-    cin >> prize.brand;
-    cout << "id type: ";
-    cin >> prize.type;
-    cout << "Now please enter the id:";
-    if(prize.type == 1)
-        cin >> prize.id_val.id_num;
+    enum week {Monday, Tuesday, Wednesday, Thirsday, Friday, Saturday, Sunday};
+    week oneday = week(4);
+    if(oneday == Friday)
+        cout << "Yes!" << endl;
     else
-        cin >> prize.id_val.id_char;
+        cout << "Nope.." << endl;
+
+    enum bits {one = 1, two = 2, four = 4, eight = 8};
+    bits myflag = bits(two);
+    myflag = bits(myflag + 4);
+    cout << myflag << endl;
 
     return 0;
 }
