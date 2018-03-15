96f14c86f18217d0b76e78121573ae2c45a39316
Julyed <274922025@qq.com>
2018-03-15 14:23:56 +0800

    cout << (int*) "hello" << endl;
    Signed-off-by: Julyed <274922025@qq.com>

--- my_own_test_1/main.cpp
+++ my_own_test_1/main.cpp
@@ -3,17 +3,7 @@ using namespace std;
 
 int main()
 {
-    enum week {Monday, Tuesday, Wednesday, Thirsday, Friday, Saturday, Sunday};
-    week oneday = week(4);
-    if(oneday == Friday)
-        cout << "Yes!" << endl;
-    else
-        cout << "Nope.." << endl;
-
-    enum bits {one = 1, two = 2, four = 4, eight = 8};
-    bits myflag = bits(two);
-    myflag = bits(myflag + 4);
-    cout << myflag << endl;
+    cout << (int *) "hahaha";
 
     return 0;
 }
