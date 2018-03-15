e1d2cf1d98593ec4f6d15af642ece7c7ea1cc39a
Julyed <274922025@qq.com>
2018-03-15 14:24:59 +0800

    为结构体类型动态分配内存
    Signed-off-by: Julyed <274922025@qq.com>

---
+++ mytask_4_15/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct fish
+{
+    string type;
+    int weight;
+    double length;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<fish> fishes(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> fishes[i].type >> fishes[i].weight >> fishes[i].length;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout << fishes[i].type << endl;
+        cout << fishes[i].weight << endl;
+        cout << fishes[i].length << endl;
+    }
+    return 0;
+}
