946096d96c02a8a2605b12287ca9ecbdf385bcb4
Julyed <274922025@qq.com>
2018-03-19 18:48:15 +0800

    螺旋队列
    Signed-off-by: Julyed <274922025@qq.com>

--- my_own_test_1/main.cpp
+++ my_own_test_1/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+
 
 int main()
 {
-    int x = (1024);
-    cout << x << endl;
+    int x, y;
+    cin >> x >> y;
+    int ceng = max(abs(x), abs(y));
+    int r = ceng * 2 -1;
+    int basic = r * r;
+
+    if(x == ceng && abs(y) < ceng)
+        cout << basic + y + ceng << endl;
+    else if(y == ceng)
+        cout << basic + 3 * ceng - x;
+    else if(x == -ceng)
+        cout << basic + 5 * ceng - y;
+    else
+        cout << basic + 7 * ceng + x;
 
     return 0;
 }
